b1f265ad4e86fc10e1cc6551dcb190ad9e0ee51e
Chiakikun <43084782+Chiakikun@users.noreply.github.com>
2020-02-23 17:03:07 +0900

    コード整形、インストール用zip追加
    コードにコメントを追加しました。
    ヘッダーやmeta.txtを修正しました。
    インストール用のzipファイルを追加しました。

--- マウスイベントサンプル/getrastervalue_sample/getrastervalue_sample.py
+++ マウスイベントサンプル/getrastervalue_sample/getrastervalue_sample.py
@@ -8,7 +8,7 @@
                               -------------------
         begin                : 2020-01-25
         git sha              : $Format:%H$
-        copyright            : (C) 2020 by unemployed
+        copyright            : (C) 2020 by Chiakikun
         email                : chiakikungm@gmail.com
  ***************************************************************************/
 
@@ -21,7 +21,7 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, QTimer
+from qgis.PyQt.QtCore import QSettings, QTimer
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction, QToolTip
 
@@ -32,96 +32,52 @@ import os.path
 import qgis.core;
 
 class GetRasterValueSample:
-    """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
 
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
         # Save reference to the QGIS interface
         self.iface = iface
+        self.canvas = self.iface.mapCanvas()
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'GetRasterValueSample_{}.qm'.format(locale))
 
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-            QCoreApplication.installTranslator(self.translator)
-
-        # Declare instance attributes
-        self.menu = self.tr(u'&GetRasterValue Sample')
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('GetRasterValueSample', message)
+        # プラグインの登録場所
+        self.menu_pos = 'サンプル マウスイベント'
+        # キャンバスウィンドウ上でのマウスイベントの設定
+        self.mouseEventSample = MouseEventSample(self.iface, self.canvas)
+        # このサンプル以外のアイコンを押した場合の設定
+        self.canvas.mapToolSet.connect(self.unsetTool)
 
 
     def initGui(self):
-        icon = QIcon(':/plugins/getrastervalue_sample/icon.png')
-        self.action = QAction(icon, self.tr(u'MapTool版サンプル'), self.iface.mainWindow())
-        self.action.triggered.connect(self.execSample)
-        self.action.setEnabled(True)
-        self.action.setCheckable(True)
-        self.action.setEnabled(True)
-        self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu(self.menu, self.action)
+        icon = QIcon(self.plugin_dir+'/icon.png')
+        self.action = QAction(icon, 'ツールチップサンプル', self.iface.mainWindow())
+        self.action.triggered.connect(self.execSample) # アイコンを押下した時に実行されるメソッドを登録
+        self.action.setCheckable(True)                 # Trueだとアイコンを押下したら次に押下するまで凹んだままになる。
+        #self.iface.addToolBarIcon(self.action)         # ツールバーにアイコンを表示させたいなら#外して
+        self.iface.addPluginToMenu(self.menu_pos, self.action)
         
-        # このサンプル実行中かな?
-        self.isrun = False
-
-        # 追加 キャンバス上のマウスイベント設定
-        self.mouseEventSample = MouseEventSample(self.iface, self.iface.mapCanvas())
-        self.iface.mapCanvas().mapToolSet.connect(self.unsetTool)
-
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
-        self.iface.removePluginMenu(
-            self.tr(u'&MapTool Sample'),
-            self.action)
+        self.iface.removePluginMenu(self.menu_pos, self.action)
         self.iface.removeToolBarIcon(self.action)
 
 
+    # このサンプル以外のアイコンが押された場合、アイコンを元の状態に戻す
     def unsetTool(self, tool):
-        try:
-        # 実行中にこのサンプル以外のアイコンを押した場合
-            if not isinstance(tool, MouseEventSample) and self.isrun:
-                self.isrun = False
-                self.action.setChecked(False)
-        except Exception:
-            pass
+        if not isinstance(tool, MouseEventSample):
+            self.action.setChecked(False)
 
 
     def execSample(self):
-        if self.isrun:
-            self.iface.mapCanvas().unsetMapTool(self.mouseEventSample)
-            self.iface.mapCanvas().setMapTool(self.previousMapTool)
-            self.isrun = False
+        if self.action.isChecked():
+            self.previousMapTool = self.canvas.mapTool()
+            self.canvas.setMapTool(self.mouseEventSample)
         else:
-            self.previousMapTool = qgis.utils.iface.mapCanvas().mapTool()
-            self.iface.mapCanvas().setMapTool(self.mouseEventSample)
-            self.isrun = True
+            self.canvas.unsetMapTool(self.mouseEventSample)
+            self.canvas.setMapTool(self.previousMapTool)
+            self.action.setChecked(False)
 
 
 class MouseEventSample(qgis.gui.QgsMapTool):
@@ -130,28 +86,31 @@ class MouseEventSample(qgis.gui.QgsMapTool):
 
         self.canvas = canvas
         self.iface = iface
+
+        # canvasMoveEventで設定した秒数（このプログラムでは700ms）経過したら呼ばれるメソッドを設定
         self.timerMapTips = QTimer( canvas )
         self.timerMapTips.timeout.connect( self.showMapTip )
+
 
     def canvasMoveEvent(self, event):
         QToolTip.hideText()
         self.timerMapTips.start( 700 )
+
 
     def showMapTip( self ):
         self.timerMapTips.stop()
 
         rLayer = self.iface.activeLayer()
         if type(rLayer) is qgis.core.QgsRasterLayer:
-            mousepos = self.canvas.mouseLastXY()
-            mappos = self.toMapCoordinates(mousepos)
+            # マウスカーソル下のピクセル値を取得する
+            mappos = self.toMapCoordinates(self.canvas.mouseLastXY())
+            ident = rLayer.dataProvider().identify( mappos, qgis.core.QgsRaster.IdentifyFormatValue ) # 参考: https://qgis.org/api/qgsraster_8h_source.html#l00057
 
-            ident = rLayer.dataProvider().identify( mappos, qgis.core.QgsRaster.IdentifyFormatValue ) # https://qgis.org/api/qgsraster_8h_source.html#l00057 ピクセル値を取得するみたいです
+            # ツールチップに表示する文字列を設定する
             if ident.isValid():
-                print('マウス位置:' + str(mousepos))
-                print('マップ位置:' + str(mappos))
-
                 values = ident.results().values()
-                text = ", ".join(['{0:g}'.format(r) for r in values if r is not None] ) # 複数取得することあるかなぁ？
+                text = ", ".join(['{0:g}'.format(r) for r in values if r is not None] ) # 複数取得することは無いだろうけど一応
             else:
                 text = "Non valid value"
+
             QToolTip.showText( self.canvas.mapToGlobal( self.canvas.mouseLastXY() ), text, self.canvas )
