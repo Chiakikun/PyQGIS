f3f9eecf8d0b890bc061b34b86bf0c4661c81ee5
Chiakikun <chiakikungm@gmail.com>
2020-06-11 22:13:27 +0900

    Create layertreeviewsample.py

---
+++ サンプル/layertreeviewsample.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ LayerTreeViewSample
+        copyright            : (C) 2020 by Chiakikun
+        email                : chiakikungm@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+使い方例（『https://github.com/Chiakikun/PyQGIS/blob/master/ダイアログ無し雛形/nodialog_skelton.py』に組み込む場合）
+
+プラグインのフォルダにこのファイルを置く
+
+インポート
+from .layertreeviewsample import LayerTreeViewSample
+
+startを次に変更する
+    def start(self):
+        self.lt = LayerTreeViewSample(self.iface)
+
+finishを次に変更する
+    def finish(self):
+        self.lt = None
+
+"""
+import qgis
+from qgis.core import *
+from qgis.gui  import *
+
+class LayerTreeViewSample:
+
+    def changeLayer(self, layer):
+        if (layer == None):
+            return
+
+        if self.currentlayer != None:
+            print(self.currentlayer.name() + 'が非アクティブになりました。')
+
+        print(layer.name()+'がアクティブになりました。')
+        self.currentlayer = layer
+
+
+    def __init__(self, iface):
+        self.iface = iface
+
+        self.currentlayer = self.iface.layerTreeView().currentLayer()
+        self.iface.layerTreeView().currentLayerChanged.connect(self.changeLayer)
+
+
+    def __del__(self):
+        self.iface.layerTreeView().currentLayerChanged.disconnect(self.changeLayer)
